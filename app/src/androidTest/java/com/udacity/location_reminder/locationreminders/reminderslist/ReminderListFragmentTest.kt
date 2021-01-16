2f999f2e74b47369b94b17a11ec11993a8755540
AnatolijsM <a.morozovs@gmail.com>
2021-01-16 22:35:46 +0200

    Fixed few UI bugs. Workig on tests

--- app/src/androidTest/java/com/udacity/location_reminder/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/location_reminder/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -1,12 +1,21 @@
 package com.udacity.location_reminder.locationreminders.reminderslist
 
 import android.app.Application
+import android.os.Bundle
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import com.udacity.location_reminder.R
 
 import com.udacity.location_reminder.locationreminders.data.ReminderDataSource
+import com.udacity.location_reminder.locationreminders.data.dto.ReminderDTO
 import com.udacity.location_reminder.locationreminders.data.local.LocalDB
 import com.udacity.location_reminder.locationreminders.data.local.RemindersLocalRepository
 import com.udacity.location_reminder.locationreminders.savereminder.SaveReminderViewModel
@@ -28,6 +37,8 @@ import org.koin.dsl.module
 import org.koin.test.AutoCloseKoinTest
 import org.koin.test.get
 import org.koin.test.inject
+import org.mockito.Mockito
+import org.mockito.Mockito.verify
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
@@ -76,11 +87,26 @@ class ReminderListFragmentTest : AutoCloseKoinTest() {
     }
 
     @Test
-    fun test() {
+    fun clickAddReminder_navigateToSaveReminderFragment() {
         val viewModel: RemindersListViewModel by inject()
         runBlockingTest {
-            viewModel.loadReminders()
+            repository.saveReminder(ReminderDTO("Title1", "Description1", "Location1", 1.0, 1.0))
         }
+
+        //Start fragment
+        val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+
+        val navController = Mockito.mock(NavController::class.java)
+        scenario.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+
+        //Then click on addReminder fab
+        Espresso.onView(withId(R.id.addReminderFAB))
+            .perform(click())
+
+        //Verify app navigated to the SaveReminderFragment
+        verify(navController).navigate(ReminderListFragmentDirections.toSaveReminder())
     }
 
 //    TODO: test the navigation of the fragments.
