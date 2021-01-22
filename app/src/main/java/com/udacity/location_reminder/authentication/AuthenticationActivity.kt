df9b65943b34e84b378db04a36b3c08a558463b5
AnatolijsM <a.morozovs@gmail.com>
2021-01-22 17:47:58 +0200

    Working on Authentication

--- app/src/main/java/com/udacity/location_reminder/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/location_reminder/authentication/AuthenticationActivity.kt
@@ -1,15 +1,18 @@
 package com.udacity.location_reminder.authentication
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.udacity.location_reminder.R
+import com.udacity.location_reminder.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -26,13 +29,17 @@ class AuthenticationActivity : AppCompatActivity() {
 
     private val TAG = AuthenticationActivity::class.java.simpleName
 
+    private val viewModel by viewModels<AuthenticationViewModel>()
 
     private val startForAuthResult = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result: ActivityResult ->
         val response = IdpResponse.fromResultIntent(result.data)
         if (result.resultCode == Activity.RESULT_OK) {
-            Log.i(TAG, "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+            Log.i(
+                TAG,
+                "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!"
+            )
         } else {
             Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
         }
@@ -42,7 +49,24 @@ class AuthenticationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
 
+        viewModel.authenticationState.observe(this) {
+            when (it) {
+                AuthenticationViewModel.AuthenticationState.AUTHENTICATED -> {
+                    Log.d(TAG, "authenticated")
+                    //
+                    val intent = Intent(this, RemindersActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    startActivity(intent)
+                }
+                else -> {
+                    Log.d(TAG, "NOT authenticated")
+                }
+            }
+        }
+
+
         launchSignInFlow()
+
     }
 
     private fun launchSignInFlow() {
